c9378288f1e5e33fbc02d62e95863b801baddfa5
FKeir <fmkeir@gmail.com>
2020-01-17 13:59:30 +0000

    Add remove_pet_by_name function

--- pet_shop.rb
+++ pet_shop.rb
@@ -36,3 +36,9 @@ def find_pet_by_name(pet_shop, pet_name)
   end
   return nil
 end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    pet_shop[:pets].delete(pet) if pet[:name] == pet_name
+  end
+end
