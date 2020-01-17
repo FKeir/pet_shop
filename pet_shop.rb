7291feea5030f2b88e3c794bbff34f3f4976d8ef
FKeir <fmkeir@gmail.com>
2020-01-17 13:51:16 +0000

    Add find_pet_by_name function

--- pet_shop.rb
+++ pet_shop.rb
@@ -29,3 +29,10 @@ def pets_by_breed(pet_shop, breed)
   end
   return pets_found
 end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    return pet if pet[:name] == pet_name
+  end
+  return nil
+end
