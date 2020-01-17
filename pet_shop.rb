4b76d0e541c61f8b6674b2be2e44b87a826d2496
FKeir <fmkeir@gmail.com>
2020-01-17 13:45:42 +0000

    Add pets_by_breed function

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,13 @@ end
 def stock_count(pet_shop)
   return pet_shop[:pets].length
 end
+
+def pets_by_breed(pet_shop, breed)
+  pets_found = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      pets_found.push(pet)
+    end
+  end
+  return pets_found
+end
